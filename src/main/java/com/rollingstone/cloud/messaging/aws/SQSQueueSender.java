de39fafb6df9c56a316a566609ea06498139d75a
Binit Datta <binitdatta@Binits-iMac.local>
2019-04-13 10:28:24 -0500

    changed

--- src/main/java/com/rollingstone/cloud/messaging/aws/SQSQueueSender.java
+++ src/main/java/com/rollingstone/cloud/messaging/aws/SQSQueueSender.java
@@ -1,5 +1,7 @@
 package com.rollingstone.cloud.messaging.aws;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.aws.messaging.core.QueueMessagingTemplate;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +11,8 @@ import com.rollingstone.command.GenericCommandResult;
 @Component
 public class SQSQueueSender {
 
+	private final static Logger log = LoggerFactory.getLogger("SQSQueueSender");
+	
 	private final QueueMessagingTemplate queueMessagingTemplate;
 	
 	public SQSQueueSender(AmazonSQSAsync amazonSqs) {
@@ -18,6 +22,7 @@ public class SQSQueueSender {
 	
 	
 	public GenericCommandResult send(String queue, Object message) {
+		log.info("The Queue name is : "+ queue);
 		this.queueMessagingTemplate.convertAndSend(queue, message);
 		GenericCommandResult<String> cr = new GenericCommandResult<String>();
 		cr.setAsSuccessful((String) message, "Success");
